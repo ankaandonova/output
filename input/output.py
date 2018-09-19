aac56ae4a892e8639fe0d882b22b170ead7ead49
ankaandonova <anka.andonova@student.hu.nl>
2018-09-19 18:02:35 +0200

    .

---
+++ input/output.py
@@ -0,0 +1,4 @@
+verdien= eval(input('Wat verdien je per uur:'))
+hoeveel= eval(input('hoeveel uur heb je gewerkt:'))
+line1= str(hoeveel)+ ' ' + 'uur werken levert ' + str(verdien*hoeveel) + ' ' + 'euro op'
+print(line1)
